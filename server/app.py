1661fd240e8cab106a35064655801c90af186d24
Alyssa <alyssa.brooksong@gmail.com>
2023-08-18 15:29:54 -0700

    routing

--- server/app.py
+++ server/app.py
@@ -2,8 +2,7 @@ from flask import Flask, jsonify, make_response, request
 from flask_cors import CORS
 from flask_migrate import Migrate
 from models import Cheese, Producer, db
-
-# from flask_restful import Api, Resource
+from flask_restful import Api, Resource
 
 
 app = Flask(__name__)
@@ -16,13 +15,29 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-# api = Api(app)
+api = Api(app)
 
 
-@app.route("/")
-def index():
-    response = make_response({"message": "Hello Fromagers!"}, 200)
-    return response
+class Producers(Resource):
+    def get(self):
+        producers = [producer.to_dict(rules=("-cheeses",)) for producer in Producer.query.all()]
+        return make_response(
+            producers,
+            200
+        )
+
+api.add_resource(Producers, "/producers")
+
+
+class ProducersByID(Resource):
+    def get(self,id):
+        producer = Producer.query.filter(Producer.id == id).first().to_dict()
+        return make_response(
+            producer,
+            200
+        )
+
+api.add_resource(ProducersByID, "/producers/<int:id>")
 
 
 if __name__ == "__main__":
--- server/models.py
+++ server/models.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy.orm import validates
 from sqlalchemy_serializer import SerializerMixin
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -10,6 +11,29 @@ class Producer(db.Model, SerializerMixin):
     __tablename__ = "producers"
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    founding_year = db.Column(db.Integer)
+    region = db.Column(db.String)
+    operation_size = db.Column(db.String)
+    image = db.Column(db.String)
+
+    cheeses = db.relationship("Cheese", back_populates="producer", cascade="all, delete-orphan")
+
+    serialize_rules = ("-cheeses.producer",)
+
+    @validates("founding_year")
+    def validate_founding_year(self,key,year):
+        if year < 1900 or year > 2023:
+            raise ValueError("Invalid founding year")
+        return year
+
+    @validates("operation_size")
+    def validate_operation_size(self,key,size):
+        if not size in ("small", "medium", "large", "family", "corporate"):
+            raise ValueError("Invalid operation size")
+        return size
+
+            
 
     def __repr__(self):
         return f"<Producer {self.id}>"
@@ -19,6 +43,29 @@ class Cheese(db.Model, SerializerMixin):
     __tablename__ = "cheeses"
 
     id = db.Column(db.Integer, primary_key=True)
+    kind = db.Column(db.String)
+    price = db.Column(db.Integer)
+    image = db.Column(db.String)
+    is_raw_milk = db.Column(db.Boolean)
+    production_date = db.Column(db.DateTime)
 
+    producer = db.relationship("Producer", back_populates="cheeses")
+    producer_id = db.Column(db.Integer, db.ForeignKey("producers.id"))
+
+    serialize_rules = ("-producer.cheeses",)
+
+
+    @validates("production_date")
+    def validate_production_date(self,key,date):
+        if date > datetime.now().date():
+            raise ValueError("Invalid date")
+        return date
+    
+    @validates("price")
+    def validte_price(self,key,price):
+        if price < 1 or price > 45.00:
+            raise ValueError("Invalid price 1-45")
+        return price
+    
     def __repr__(self):
         return f"<Cheese {self.id}>"
