1b9ae2b2878ef31631216778c4f8de989e94fca3
Alyssa <alyssa.brooksong@gmail.com>
2023-08-21 19:32:42 -0700

    completed assignment

--- server/app.py
+++ server/app.py
@@ -1,9 +1,11 @@
+from datetime import datetime
+
 from flask import Flask, jsonify, make_response, request
 from flask_cors import CORS
 from flask_migrate import Migrate
-from models import Cheese, Producer, db
 from flask_restful import Api, Resource
-
+from models import Cheese, Producer, db
+from werkzeug.exceptions import NotFound
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
@@ -18,30 +20,92 @@ db.init_app(app)
 api = Api(app)
 
 
+# @app.route("/")
+# def index():
+#     response = make_response({"message": "Hello Fromagers!"}, 200)
+#     return response
+
+
 class Producers(Resource):
     def get(self):
-        producers = [producer.to_dict(rules=("-cheeses",)) for producer in Producer.query.all()]
-        return make_response(
-            producers,
-            200
+        producers = [
+            producer.to_dict(rules=("-cheeses",)) for producer in Producer.query.all()
+        ]
+        response = make_response(jsonify(producers), 200)
+        return response
+
+
+class ProducersById(Resource):
+    def get(self, id):
+        producer = Producer.query.get_or_404(id)
+        response = make_response(jsonify(producer.to_dict()), 200)
+        return response
+
+    def delete(self, id):
+        producer = Producer.query.get_or_404(id)
+        db.session.delete(producer)
+        db.session.commit()
+        response = make_response("", 204)
+        return response
+
+
+class Cheeses(Resource):
+    def post(self):
+        data = request.get_json()
+        try:
+            cheese = Cheese(**data)
+        except ValueError as e:
+            response = make_response(jsonify({"error": e.args}), 422)
+            return response
+
+        db.session.add(cheese)
+        db.session.commit()
+        response = make_response(
+            jsonify(
+                cheese.to_dict(
+                    rules=(
+                        "-producer.founding_year",
+                        "-producer.region",
+                        "-producer.operation_size",
+                        "-producer.image",
+                        "-producer.id",
+                    )
+                )
+            ),
+            201,
         )
+        return response
+
+
+class CheesesById(Resource):
+    def patch(self, id):
+        cheese = Cheese.query.get_or_404(id)
+        data = request.get_json()
+        for key, value in data.items():
+            if key == "production_date":
+                value = datetime.strptime(value, "%Y-%m-%d")
+            setattr(cheese, key, value)
+        db.session.commit()
+        response = make_response(jsonify(cheese.to_dict(rules=("-producer",))), 200)
+        return response
+
+    def delete(self, id):
+        cheese = Cheese.query.get_or_404(id)
+        db.session.delete(cheese)
+        db.session.commit()
+        response = make_response("", 204)
+        return response
+
 
 api.add_resource(Producers, "/producers")
+api.add_resource(ProducersById, "/producers/<int:id>")
+api.add_resource(Cheeses, "/cheeses")
+api.add_resource(CheesesById, "/cheeses/<int:id>")
 
 
-class ProducersByID(Resource):
-    def get(self,id):
-        producer = Producer.query.filter(Producer.id == id).first().to_dict()
-        return make_response(
-            producer,
-            200
-        )
-    def delete(self,id):
-        producer = Producer.query.filter(Producer.id == id).first()
-        if not producer:
-            abort(404, "Resource not found")
-
-api.add_resource(ProducersByID, "/producers/<int:id>")
+@app.errorhandler(NotFound)
+def handle_not_found(error):
+    return make_response(jsonify({"error": "Resource not found"}), 404)
 
 
 if __name__ == "__main__":
--- server/models.py
+++ server/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy.orm import validates
 from sqlalchemy_serializer import SerializerMixin
@@ -6,8 +7,13 @@ from datetime import datetime
 
 db = SQLAlchemy()
 
+class TimestampMixin:
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(
+        db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now()
+     )
 
-class Producer(db.Model, SerializerMixin):
+class Producer(db.Model, SerializerMixin, TimestampMixin):
     __tablename__ = "producers"
 
     id = db.Column(db.Integer, primary_key=True)
@@ -17,9 +23,9 @@ class Producer(db.Model, SerializerMixin):
     operation_size = db.Column(db.String)
     image = db.Column(db.String)
 
-    cheeses = db.relationship("Cheese", back_populates="producer", cascade="all, delete-orphan")
+    cheeses = db.relationship("Cheese", back_populates="producer", lazy=True, cascade="delete")
 
-    serialize_rules = ("-cheeses.producer",)
+    serialize_rules = ("-cheeses.producer","-created_at", "-updated_at")
 
     @validates("founding_year")
     def validate_founding_year(self,key,year):
@@ -33,8 +39,6 @@ class Producer(db.Model, SerializerMixin):
             raise ValueError("Invalid operation size")
         return size
 
-            
-
     def __repr__(self):
         return f"<Producer {self.id}>"
 
@@ -44,27 +48,27 @@ class Cheese(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     kind = db.Column(db.String)
-    price = db.Column(db.Integer)
+    price = db.Column(db.Float)
     image = db.Column(db.String)
     is_raw_milk = db.Column(db.Boolean)
     production_date = db.Column(db.DateTime)
 
-    producer = db.relationship("Producer", back_populates="cheeses")
-    producer_id = db.Column(db.Integer, db.ForeignKey("producers.id"))
+    producer_id = db.Column(db.Integer, db.ForeignKey("producers.id"), nullable=False)
 
-    serialize_rules = ("-producer.cheeses",)
+    serialize_rules = ("-producers.cheeses","-created_at", "-updated_at")
 
 
     @validates("production_date")
     def validate_production_date(self,key,date):
-        if date > datetime.now().date():
-            raise ValueError("Invalid date")
-        return date
+        production_date = datetime.strptime(date, "%Y-%m-%d")
+        if production_date >= datetime.now():
+            raise ValueError("Invalid date, msut be in the past")
+        return production_date
     
     @validates("price")
-    def validte_price(self,key,price):
-        if price < 1 or price > 45.00:
-            raise ValueError("Invalid price 1-45")
+    def validate_price(self,key,price):
+        if not 1.00 <= price <= 45.00:
+            raise ValueError("Price must be between 1.00 and 45.00")
         return price
     
     def __repr__(self):
